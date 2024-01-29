0e8a8f97716baf9bbe80d5fd137a6a96d881c7f5
Josenilsonfariasx <fariaslwork@gmail.com>
2024-01-29 20:39:00 -0300

    feat: init config from server

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express, {Request, Response, NextFunction} from "express"
+import cors from "cors"
+
+const app = express()
+app.use(cors())
+
+app.use((err:Error, req:Request, res:Response, next:NextFunction)=>{
+  if(err instanceof Error){
+    return res.status(400).json({error: err.message})
+}
+  return res.status(500).json({
+    status: 'Error',
+    message: 'Internal Server Error'
+  })
+})
+
+app.listen(3333, ()=>{
+  console.log('Jornal On!')
+})
