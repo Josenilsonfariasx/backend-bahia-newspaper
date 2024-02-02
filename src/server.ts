abfe82f4ffd0407ab2a4dadd07d120afe8dd04cc
Josenilsonfariasx <fariaslwork@gmail.com>
2024-02-02 08:17:38 -0300

    fix: config from server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express, {Request, Response, NextFunction} from "express"
 import cors from "cors"
+import { routes } from "./routes"
 
 const app = express()
+app.use(express.json())
 app.use(cors())
+app.use(routes)
 
 app.use((err:Error, req:Request, res:Response, next:NextFunction)=>{
   if(err instanceof Error){
@@ -13,7 +16,6 @@ app.use((err:Error, req:Request, res:Response, next:NextFunction)=>{
     message: 'Internal Server Error'
   })
 })
-
 app.listen(3333, ()=>{
   console.log('Jornal On!')
 })
