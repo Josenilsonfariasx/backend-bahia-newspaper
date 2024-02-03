e296cc1d74ceeb04c047f90d03068f622535b0d4
Josenilsonfariasx <fariaslwork@gmail.com>
2024-02-03 17:08:54 -0300

    feat: adding routes

--- src/routes/user/user.routes.ts
+++ src/routes/user/user.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { CreateUserController } from "../../controllers/user/CreateUserController";
+import { ListUserController } from "../../controllers/user/ListUserController";
 
 export const usersRoutes: Router = Router();
 
 usersRoutes.post('/', new CreateUserController().handle)
+usersRoutes.get('/', new ListUserController().handle)
