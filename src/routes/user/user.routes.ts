282dd0769cc105204d9f83f27bfb7dc33290c061
Josenilsonfariasx <fariaslwork@gmail.com>
2024-02-04 09:58:02 -0300

    feat: adding route

--- src/routes/user/user.routes.ts
+++ src/routes/user/user.routes.ts
@@ -4,6 +4,7 @@ import { ListUserController } from "../../controllers/user/ListUserController";
 import { ListUserByIdController } from "../../controllers/user/ListUserByIdController";
 import { DeleteUserController } from "../../controllers/user/DeleteUserController";
 import { UpdateUserController } from "../../controllers/user/UpdateUserController";
+import { AuthUserController } from "../../controllers/user/AuthUserController";
 
 export const usersRoutes: Router = Router();
 
@@ -12,3 +13,5 @@ usersRoutes.get('/', new ListUserController().handle)
 usersRoutes.get('/:id', new ListUserByIdController().handle)
 usersRoutes.delete('/:id', new DeleteUserController().handle)
 usersRoutes.put('/:id', new UpdateUserController().handle)
+
+usersRoutes.post('/session', new AuthUserController().handle)
