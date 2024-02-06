b48b5893d55d5ab5ec28a4c16d8c36fc8394dd53
Josenilsonfariasx <fariaslwork@gmail.com>
2024-02-06 03:57:13 -0300

    feat: adding function send email

---
+++ src/utils/send.ts
@@ -0,0 +1,20 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  service: 'smtp', // Altere para o serviço de e-mail desejado, como 'gmail', 'hotmail', etc.
+  host: process.env.MAIL_HOST,
+  port: Number(process.env.MAIL_PORT),
+  secure: false, // Defina como true se estiver usando SSL/TLS
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+export const send = (to, subject, body)=>{
+  transporter.sendMail({
+    from: process.env.MAIL_FROM,
+    to,
+    subject,
+    html: body
+  })
+}
