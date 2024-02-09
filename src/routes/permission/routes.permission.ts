77474f49fa48151344b5992a7a5073f81a9e18ff
Josenilsonfariasx <fariaslwork@gmail.com>
2024-02-09 11:16:08 -0300

    feat: adding route

--- src/routes/permission/routes.permission.ts
+++ src/routes/permission/routes.permission.ts
@@ -4,10 +4,12 @@ import { isAuthenticated } from "../../middlewares/isAuthenticated";
 import { InsertPermissionForUserController } from "../../controllers/permissions/InsertPermissionForUserController";
 import { InsertPermissionForRoleController } from "../../controllers/permissions/InsertPermissionForRoleController";
 import { RemovePermissionUserController } from "../../controllers/permissions/RemovePermissionUserController";
+import { RemovePermissionRoleController } from "../../controllers/permissions/RemovePermissionRoleController";
 
 export const permissionRoutes: Router = Router()
 
 permissionRoutes.post('/', isAuthenticated, new CreatePermissionController().handle)
 permissionRoutes.delete('/user', isAuthenticated, new RemovePermissionUserController().handle)
+permissionRoutes.delete('/role', isAuthenticated, new RemovePermissionRoleController().handle)
 permissionRoutes.post('/user', isAuthenticated, new InsertPermissionForUserController().handle)
 permissionRoutes.post('/role', isAuthenticated, new InsertPermissionForRoleController().handle)
