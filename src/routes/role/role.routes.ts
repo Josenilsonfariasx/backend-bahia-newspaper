a85577acac798f1b0b22a0fffa9605d4ff40f576
Josenilsonfariasx <fariaslwork@gmail.com>
2024-02-12 07:41:32 -0300

    feat: adding routes

--- src/routes/role/role.routes.ts
+++ src/routes/role/role.routes.ts
@@ -1,7 +1,17 @@
 import { Router } from "express";
 import { CreateRoleController } from "../../controllers/role/CreateRoleController";
 import { isAuthenticated } from "../../middlewares/isAuthenticated";
+import { ListRoleController } from "../../controllers/role/ListRoleController";
 
 export const roleRoutes: Router = Router()
 
+// post
 roleRoutes.post('/', isAuthenticated, new CreateRoleController().handle)
+
+// get
+roleRoutes.get('/', isAuthenticated, new ListRoleController().handle)
+
+// delete
+
+
+// put
