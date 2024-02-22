82806bb4183c50aa385a84c989ef2e1106722b2d
Josenilsonfariasx <fariaslwork@gmail.com>
2024-02-22 14:26:56 -0300

    feat: adding routes

--- src/routes/category/category.routes.ts
+++ src/routes/category/category.routes.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
 import { CreateCategoryController } from "../../controllers/category/CreateCategoryController";
 import { isAuthenticated } from "../../middlewares/isAuthenticated";
+import { ListCategoryController } from "../../controllers/category/ListCategoryController";
+import { UpdateCategoryController } from "../../controllers/category/UpdateCategoryController";
+import { DeleteCategoryController } from "../../controllers/category/DeleteCategoryController";
 
 export const categoryRoutes = Router();
 
 // post
 categoryRoutes.post('/', isAuthenticated, new CreateCategoryController().handle)
+// get
+categoryRoutes.get('/', isAuthenticated, new ListCategoryController().handle)
+// put
+categoryRoutes.put('/', isAuthenticated, new UpdateCategoryController().handle)
+// delete
+categoryRoutes.delete('/', isAuthenticated, new DeleteCategoryController().handle)
