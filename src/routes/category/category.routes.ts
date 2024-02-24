851827c73973bf5cd722432178608958c7d7defe
Josenilsonfariasx <fariaslwork@gmail.com>
2024-02-24 08:43:07 -0300

    feat: adding routes

--- src/routes/category/category.routes.ts
+++ src/routes/category/category.routes.ts
@@ -4,6 +4,7 @@ import { isAuthenticated } from "../../middlewares/isAuthenticated";
 import { ListCategoryController } from "../../controllers/category/ListCategoryController";
 import { UpdateCategoryController } from "../../controllers/category/UpdateCategoryController";
 import { DeleteCategoryController } from "../../controllers/category/DeleteCategoryController";
+import { GetCategoryByIdController } from "../../controllers/category/GetCategoryByIdController";
 
 export const categoryRoutes = Router();
 
@@ -11,6 +12,7 @@ export const categoryRoutes = Router();
 categoryRoutes.post('/', isAuthenticated, new CreateCategoryController().handle)
 // get
 categoryRoutes.get('/', isAuthenticated, new ListCategoryController().handle)
+categoryRoutes.get('/:id', isAuthenticated, new GetCategoryByIdController().handle)
 // put
 categoryRoutes.put('/', isAuthenticated, new UpdateCategoryController().handle)
 // delete
