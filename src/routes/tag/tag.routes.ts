917a0579fad43914f3ebbcc98bf7a1cfa417e86e
Josenilsonfariasx <fariaslwork@gmail.com>
2024-02-24 09:11:47 -0300

    feat: adding routes

--- src/routes/tag/tag.routes.ts
+++ src/routes/tag/tag.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import { isAuthenticated } from "../../middlewares/isAuthenticated";
-import { CreateTagController } from "../../controllers/Tag/CreateTagController";
+import { CreateTagController } from "../../controllers/tag/CreateTagController";
+import { ListTagsController } from "../../controllers/tag/ListTagsController";
 
 export const TagRoutes = Router()
 
+// post
 TagRoutes.post('/', isAuthenticated, new CreateTagController().handle)
+
+// get
+TagRoutes.get('/', isAuthenticated, new ListTagsController().handle)
