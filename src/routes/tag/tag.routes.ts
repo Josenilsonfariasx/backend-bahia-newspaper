41dc0b04e0c2c637035dc216eb2d0a5a9387f884
Josenilsonfariasx <fariaslwork@gmail.com>
2024-02-26 13:33:08 -0300

    feat: adding routes

--- src/routes/tag/tag.routes.ts
+++ src/routes/tag/tag.routes.ts
@@ -3,6 +3,7 @@ import { isAuthenticated } from "../../middlewares/isAuthenticated";
 import { CreateTagController } from "../../controllers/tag/CreateTagController";
 import { ListTagsController } from "../../controllers/tag/ListTagsController";
 import { UpdateTagController } from "../../controllers/tag/UpdateTagController";
+import { DeleteTagController } from "../../controllers/tag/DeleteTagController";
 
 export const TagRoutes = Router()
 
@@ -14,3 +15,6 @@ TagRoutes.get('/', isAuthenticated, new ListTagsController().handle)
 
 // put
 TagRoutes.put('/', isAuthenticated, new UpdateTagController().handle)
+
+// delete
+TagRoutes.delete('/:id', isAuthenticated, new DeleteTagController().handle)
