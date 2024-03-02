fa28ca64f4c3b1266630b9193a90f87e5ac4c5ee
Josenilsonfariasx <fariaslwork@gmail.com>
2024-03-02 19:47:59 -0300

    feat: adding routes

--- src/routes/post/post.routes.ts
+++ src/routes/post/post.routes.ts
@@ -8,6 +8,7 @@ import { UpdatePostController } from "../../controllers/post/UpdatePostControlle
 import { DeletePostController } from "../../controllers/post/DeletePostController";
 import { GetPostByIdController } from "../../controllers/post/GetPostByIdController";
 import { UpdateStatusPostController } from "../../controllers/post/UpdateStatusPostController";
+import { DeleteImageFromPostController } from "../../controllers/post/DeleteImageFromPostController";
 
 
 export const postRoutes = Router()
@@ -15,15 +16,16 @@ export const postRoutes = Router()
 
 // post
 const upload = multer(uploadConfig);
-postRoutes.post('/',  upload.array('files', 10), isAuthenticated, new CreatePostController().handle)
+postRoutes.post('/', isAuthenticated, upload.array('files', 10), isAuthenticated, new CreatePostController().handle)
 
 // get
-postRoutes.get('/', new ListAllPostController().handle)
-postRoutes.get('/:id', new GetPostByIdController().handle)
+postRoutes.get('/', isAuthenticated, new ListAllPostController().handle)
+postRoutes.get('/:id', isAuthenticated, new GetPostByIdController().handle)
 
 // put
-postRoutes.put('/:id', new UpdatePostController().handle)
-postRoutes.put('/status/:id', new UpdateStatusPostController().handle)
+postRoutes.put('/:id', isAuthenticated, new UpdatePostController().handle)
+postRoutes.put('/status/:id', isAuthenticated, new UpdateStatusPostController().handle)
 
 // delete
-postRoutes.delete('/:id', new DeletePostController().handle)
+postRoutes.delete('/:id', isAuthenticated, new DeletePostController().handle)
+postRoutes.delete('/image/delete/:id', isAuthenticated, new DeleteImageFromPostController().handle)
