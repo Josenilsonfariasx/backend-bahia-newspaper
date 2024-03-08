4cb311fb448fffa1a3c8afce0e0413458c5d401c
Josenilsonfariasx <fariaslwork@gmail.com>
2024-03-07 21:38:36 -0300

    fix: function redis

--- src/controllers/post/CancelAppointmentPostController.ts
+++ src/controllers/post/CancelAppointmentPostController.ts
@@ -1,13 +1,13 @@
-import { Request, Response } from "express";
-import { CancelAppointmentPostService } from "../../services/post/CancelAppointmentPostService";
+// import { Request, Response } from "express";
+// import { CancelAppointmentPostService } from "../../services/post/CancelAppointmentPostService";
 
-class CancelAppointmentPostController {
-  async handle(req: Request, res: Response) {
-  const { id } = req.params
-  const cancelAppointmentPostService = new CancelAppointmentPostService()
-  const post = await cancelAppointmentPostService.execute({ id })
-  return res.json(post)
-  }
-}
+// class CancelAppointmentPostController {
+//   async handle(req: Request, res: Response) {
+//   const { id } = req.params
+//   const cancelAppointmentPostService = new CancelAppointmentPostService()
+//   const post = await cancelAppointmentPostService.execute({ id })
+//   return res.json(post)
+//   }
+// }
 
-export { CancelAppointmentPostController };
+// export { CancelAppointmentPostController };
--- src/controllers/post/CreatePostScheduleController.ts
+++ src/controllers/post/CreatePostScheduleController.ts
@@ -1,14 +1,14 @@
-import { Request, Response } from "express";
-import { CreatePostScheduleService } from "../../services/post/CreatePostScheduleService";
+// import { Request, Response } from "express";
+// import { CreatePostScheduleService } from "../../services/post/CreatePostScheduleService";
 
-class CreatePostScheduleController {
-  async handle(req: Request, res: Response) {
-    const { title, content, scheduledAt } = req.body
-    const files: Express.Multer.File[] = req.files as Express.Multer.File[]
-    const createPostScheduleService = new CreatePostScheduleService()
-    const post = await createPostScheduleService.execute({ title, content, scheduledAt, files})
-    return res.json(post)
-  }
-}
+// class CreatePostScheduleController {
+//   async handle(req: Request, res: Response) {
+//     const { title, content, scheduledAt } = req.body
+//     const files: Express.Multer.File[] = req.files as Express.Multer.File[]
+//     const createPostScheduleService = new CreatePostScheduleService()
+//     const post = await createPostScheduleService.execute({ title, content, scheduledAt, files})
+//     return res.json(post)
+//   }
+// }
 
-export { CreatePostScheduleController };
+// export { CreatePostScheduleController };
