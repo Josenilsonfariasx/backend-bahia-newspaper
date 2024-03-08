5792abfb2956cbfb9a7e1bff1f796540b36fa6e2
Josenilsonfariasx <fariaslwork@gmail.com>
2024-03-08 11:58:51 -0300

    fix: logic for delete files

--- src/configs/multerConfig.ts
+++ src/configs/multerConfig.ts
@@ -1,6 +1,7 @@
 import multer from 'multer';
 import path from 'path';
 import crypto from 'crypto';
+import fs from 'fs';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
@@ -15,4 +16,31 @@ export default {
       return callback(null, filename);
     },
   }),
+  fileFilter: (req: any, file: any, cb: any) => {
+    const allowedMimes = [
+      'image/jpeg',
+      'image/pjpeg',
+      'image/png',
+      'image/gif',
+      'video/x-matroska',
+      'video/mp4',
+    ];
+
+    if (allowedMimes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error('Invalid file type.'));
+    }
+  },
+  cleanup: () => {
+    fs.readdir(tmpFolder, (err, files) => {
+      if (err) throw err;
+
+      for (const file of files) {
+        fs.unlink(path.join(tmpFolder, file), err => {
+          if (err) throw err;
+        });
+      }
+    });
+  },
 };
