73e82f6c58bbafe57480ec8f8503010ec85e39ca
Josenilsonfariasx <fariaslwork@gmail.com>
2024-04-01 09:18:52 -0300

    fix:v7

--- src/services/aws/DeleteImageService.ts
+++ src/services/aws/DeleteImageService.ts
@@ -1,25 +1,25 @@
-// import S3Storage from "../../utils/S3Storage";
+import S3Storage from "../../utils/S3Storage";
 
-// class DeleteImageService {
-//   async execute(filenames: string | string[]) {
-//     try {
-//       const s3 = S3Storage.getInstance();
-//       console.log(filenames)
+class DeleteImageService {
+  async execute(filenames: string | string[]) {
+    try {
+      const s3 = S3Storage.getInstance();
+      console.log(filenames)
   
-//       if (typeof filenames === 'string') {
-//         // Se filenames for uma única string, delete o arquivo e retorne um array com o resultado
-//         const file = await s3.deleteFile(filenames);
-//         return [file];
-//       } else {
-//         // Se filenames for um array de strings, delete todos os arquivos e retorne um array com os resultados
-//         const deletePromises = filenames.map(filename => s3.deleteFile(filename));
-//         const files = await Promise.all(deletePromises);
-//         return files;
-//       }
-//     } catch (error) {
-//       console.log('Error during delete image: ', error.message);
-//     }
-//   }
-// }
+      if (typeof filenames === 'string') {
+        // Se filenames for uma única string, delete o arquivo e retorne um array com o resultado
+        const file = await s3.deleteFile(filenames);
+        return [file];
+      } else {
+        // Se filenames for um array de strings, delete todos os arquivos e retorne um array com os resultados
+        const deletePromises = filenames.map(filename => s3.deleteFile(filename));
+        const files = await Promise.all(deletePromises);
+        return files;
+      }
+    } catch (error) {
+      console.log('Error during delete image: ', error.message);
+    }
+  }
+}
 
-// export { DeleteImageService}
+export { DeleteImageService}
--- src/services/post/CreatePostService.ts
+++ src/services/post/CreatePostService.ts
@@ -1,58 +1,58 @@
-// import multer from 'multer'
-// // import UploadImagesService from '../aws/uploadImagesService';
-// import prismaClient from '../../prisma';
+import multer from 'multer'
+import UploadImagesService from '../aws/uploadImagesService';
+import prismaClient from '../../prisma';
 
-// interface PostRequest {
-//   title: string;
-//   content: string;
-//   files: Express.Multer.File[]
-// }
+interface PostRequest {
+  title: string;
+  content: string;
+  files: Express.Multer.File[]
+}
 
-// class CreatePostService {
-//   async execute({ title, content, files }: PostRequest) {
-//     try {
-//       if(!title || !content) throw new Error('Title and Content are required')
+class CreatePostService {
+  async execute({ title, content, files }: PostRequest) {
+    try {
+      if(!title || !content) throw new Error('Title and Content are required')
 
-//       // const uploadService = new UploadImagesService();
+      const uploadService = new UploadImagesService();
 
-//       // const uploadFile = async (file: Express.Multer.File) => {
-//       //   const fileUrl = await uploadService.execute(file);
-//       //   return { filename: file.originalname, url: fileUrl };
-//       // }
+      const uploadFile = async (file: Express.Multer.File) => {
+        const fileUrl = await uploadService.execute(file);
+        return { filename: file.originalname, url: fileUrl };
+      }
 
-//       const [photos, videos] = files.reduce((acc, file) => {
-//         if (file.mimetype.startsWith('image')) {
-//           acc[0].push(file);
-//         } else if (file.mimetype.startsWith('video')) {
-//           acc[1].push(file);
-//         }
-//         return acc;
-//       }, [[], []]);
+      const [photos, videos] = files.reduce((acc, file) => {
+        if (file.mimetype.startsWith('image')) {
+          acc[0].push(file);
+        } else if (file.mimetype.startsWith('video')) {
+          acc[1].push(file);
+        }
+        return acc;
+      }, [[], []]);
 
-//       // const [uploadedPhotos, uploadedVideos] = await Promise.all([
-//       //   Promise.all(photos.map(uploadFile)),
-//       //   Promise.all(videos.map(uploadFile))
-//       // ]);
+      const [uploadedPhotos, uploadedVideos] = await Promise.all([
+        Promise.all(photos.map(uploadFile)),
+        Promise.all(videos.map(uploadFile))
+      ]);
 
-//       const post = await prismaClient.post.create({
-//         data:{
-//           title,
-//           content,
-//           // photoUrls: uploadedPhotos.map(({ url }) => url),
-//           // videoUrls: uploadedVideos.map(({ url }) => url),
-//           published: true,
-//           publishedAt: new Date()
-//         }, include: {
-//           categories: true,
-//           tags: true
-//         }
-//       })
+      const post = await prismaClient.post.create({
+        data:{
+          title,
+          content,
+          photoUrls: uploadedPhotos.map(({ url }) => url),
+          videoUrls: uploadedVideos.map(({ url }) => url),
+          published: true,
+          publishedAt: new Date()
+        }, include: {
+          categories: true,
+          tags: true
+        }
+      })
 
-//       return post;
-//     } catch (error) {
-//       console.error('Erro durante o upload:', error);
-//       throw new Error('Erro durante o upload'+ error.message);
-//     }
-//   }
-// }
-// export { CreatePostService };
+      return post;
+    } catch (error) {
+      console.error('Erro durante o upload:', error);
+      throw new Error('Erro durante o upload'+ error.message);
+    }
+  }
+}
+export { CreatePostService };
