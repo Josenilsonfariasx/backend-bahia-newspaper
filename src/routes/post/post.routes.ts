0f09a80437eae41be50d33a8277572a95792fef8
Josenilsonfariasx <fariaslwork@gmail.com>
2024-04-01 08:52:16 -0300

    FIX: MMMM

--- src/routes/post/post.routes.ts
+++ src/routes/post/post.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { isAuthenticated } from "../../middlewares/isAuthenticated";
-import { CreatePostController } from "../../controllers/post/CreatePostController";
+// import { CreatePostController } from "../../controllers/post/CreatePostController";
 import multer from "multer";
 import uploadConfig from '../../configs/multerConfig';
 import { ListAllPostController } from "../../controllers/post/ListAllPostController";
@@ -23,7 +23,7 @@ export const postRoutes = Router()
 // post
 const upload = multer(uploadConfig);
 
-postRoutes.post('/', isAuthenticated, upload.array('files', 10), isAuthenticated, new CreatePostController().handle)
+// postRoutes.post('/', isAuthenticated, upload.array('files', 10), isAuthenticated, new CreatePostController().handle)
 // postRoutes.post('/schedule', isAuthenticated, upload.array('files', 10), isAuthenticated, new CreatePostScheduleController().handle)
 postRoutes.post('/category/:id', isAuthenticated, new InsertCategoryInPostController().handle)
 postRoutes.post('/tag/:id', isAuthenticated, new InsertTagInPostController().handle)
