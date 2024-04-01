425df337aaaca28022b9326ef5ffd4c8ac5a9615
Josenilsonfariasx <fariaslwork@gmail.com>
2024-04-01 09:33:29 -0300

    fx,

--- src/services/aws/DeleteImageService.ts
+++ src/services/aws/DeleteImageService.ts
@@ -1,24 +1,24 @@
-import S3Storage from "../../utils/S3Storage";
+// import S3Storage from "../../utils/S3Storage";
 
-const s3 = S3Storage.getInstance();
+// const s3 = S3Storage.getInstance();
 
-class DeleteImageService {
-  async execute(filenames: string | string[]) {
-    try {
-      if (typeof filenames === 'string') {
-        // Se filenames for uma única string, delete o arquivo e retorne um array com o resultado
-        const file = await s3.deleteFile(filenames);
-        return [file];
-      } else {
-        // Se filenames for um array de strings, delete todos os arquivos e retorne um array com os resultados
-        const deletePromises = filenames.map(filename => s3.deleteFile(filename));
-        const files = await Promise.all(deletePromises);
-        return files;
-      }
-    } catch (error) {
-      console.log('Error during delete image: ', error.message);
-    }
-  }
-}
+// class DeleteImageService {
+//   async execute(filenames: string | string[]) {
+//     try {
+//       if (typeof filenames === 'string') {
+//         // Se filenames for uma única string, delete o arquivo e retorne um array com o resultado
+//         const file = await s3.deleteFile(filenames);
+//         return [file];
+//       } else {
+//         // Se filenames for um array de strings, delete todos os arquivos e retorne um array com os resultados
+//         const deletePromises = filenames.map(filename => s3.deleteFile(filename));
+//         const files = await Promise.all(deletePromises);
+//         return files;
+//       }
+//     } catch (error) {
+//       console.log('Error during delete image: ', error.message);
+//     }
+//   }
+// }
 
-export { DeleteImageService}
+// export { DeleteImageService}
