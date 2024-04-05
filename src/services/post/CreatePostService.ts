e4d6f857244d99249617c64ac5b0f201328f6dcc
Josenilsonfariasx <fariaslwork@gmail.com>
2024-04-04 23:42:03 -0300

    fix: teste

--- src/services/post/CreatePostService.ts
+++ src/services/post/CreatePostService.ts
@@ -8,21 +8,21 @@ interface PostRequest {
   file: Express.Multer.File
 }
 
-const uploadService = new UploadImagesService();
+// const uploadService = new UploadImagesService();
 
 class CreatePostService {
   async execute({ title, content, file }: PostRequest) {
     try {
       if(!title || !content) throw new Error('Title and Content are required')
 
-      const fileUrl = await uploadService.execute(file);
-      const uploadedFile = { filename: file.originalname, url: fileUrl };
+      // const fileUrl = await uploadService.execute(file);
+      // const uploadedFile = { filename: file.originalname, url: fileUrl };
 
       const post = await prismaClient.post.create({
         data:{
           title,
           content,
-          photoUrls: [uploadedFile.url],
+          // photoUrls: [uploadedFile.url],
           published: true,
           publishedAt: new Date()
         }, include: {
