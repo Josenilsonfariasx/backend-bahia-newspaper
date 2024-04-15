a0efca3ca3d7652205e692db25584c5a90aa3a8e
Josenilsonfariasx <fariaslwork@gmail.com>
2024-04-15 14:07:05 -0300

    feat: adding function for add count in post

--- src/services/post/GetPostByIdService.ts
+++ src/services/post/GetPostByIdService.ts
@@ -8,8 +8,9 @@ class GetPostByIdService {
     try {
       if(!id) throw new Error('Id from Post is required')
       const post = await prismaClient.post.findFirst({ where: { id }, include: {tags: true, categories:true}})
+      const postUpdated = await prismaClient.post.update({where: {id}, data: {post_view_count: post.post_view_count+1}, include: {tags: true, categories:true}})
       if(!post) throw new Error('Post not found')
-      return post
+      return postUpdated
     } catch (error) {
       throw new Error('Failed to get post: '+error.message)
     }
