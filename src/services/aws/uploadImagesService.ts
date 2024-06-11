0994f87856920e0ac97013c4d128d0f5cdc76045
Josenilsonfariasx <fariaslwork@gmail.com>
2024-06-11 18:28:21 -0300

    fix: url-foto

--- src/services/aws/uploadImagesService.ts
+++ src/services/aws/uploadImagesService.ts
@@ -1,13 +1,13 @@
-import S3Storage from '../../utils/S3Storage';
+import S3Storage from "../../utils/S3Storage";
 
 const s3 = S3Storage.getInstance();
 
 class UploadImagesService {
   async execute(file: Express.Multer.File): Promise<string> {
-    const fileUrl = await s3.saveFile(file.filename)
+    const filename = file.filename.replace(/\s/g, "");
+    const fileUrl = await s3.saveFile(filename);
     // Retorna a URL
     return fileUrl;
   }
 }
-
 export default UploadImagesService;
