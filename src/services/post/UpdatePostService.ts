d8417dc5f060baa61c3c18c478b8b4ffbb36ac07
Josenilsonfariasx <fariaslwork@gmail.com>
2024-06-29 10:57:17 -0300

    fix: logic update post

--- src/services/post/UpdatePostService.ts
+++ src/services/post/UpdatePostService.ts
@@ -11,7 +11,8 @@ class UpdatePostService {
   async execute({ id, title, content, categoryId }: PostRequest) {
     try {
       if (!id) throw new Error("Id from Post is required");
-      if (!title || !content) throw new Error("Title and Content from Post are required");
+      if (!title && !content && categoryId)
+        throw new Error("Title and Content and Category from Post are required");
 
       const post = await prismaClient.post.findFirst({ where: { id }, include: { categories: true } });
       if (!post) throw new Error("Post not found");
